2d4287965ae98f0d2d54eda3288e6a89a17c3f30
Danny Gilbert <spydercapriani@gmail.com>
2014-09-25 18:16:41 -0500

    User Interface Functionality Commit
    Finalized user interface validation

--- Lemonade Stand/ViewController.swift
+++ Lemonade Stand/ViewController.swift
@@ -22,28 +22,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepperPurchIce: UIStepper!
     @IBOutlet weak var stepperMixLemon: UIStepper!
     @IBOutlet weak var stepperMixIce: UIStepper!
+    @IBOutlet weak var btnStartDay: UIButton!
     
     // Constants
     let costLemons:Int = 2
     let costIce:Int = 1
     
     // Variables
-    var oldLemonsPurch = 0
-    var oldIcePurch = 0
-    var oldLemonsMix = 0
-    var oldIceMix = 0
-    
-    var lemonMix:Int = 0
+    var inventory = Supplies(money: 10, lemons: 1, iceCubes: 0)
+    var lemonMix:Int = 1
     var iceMix:Int = 0
-    
-    var inventory = Supplies(money: 10, lemons: 0, iceCubes: 0)
-    
-    var totalCost:Int = 0
+    var mixRatio:Double = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if inventory.money == 0 && inventory.lemons == 0 && inventory.iceCubes == 0{ // Reset Game if User runs out of credits/supplies
+            showMessageAlert(header: "Game Over", message: "You ran out of money and supplies! Play Again?")
+            inventory = Supplies(money: 10, lemons: 1, iceCubes: 0)
+        }
+        
         updateSupplyView()
+        lemonMix = 1
+        iceMix = 0
+        updateMixView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -67,8 +69,12 @@ class ViewController: UIViewController {
             inventory.lemons--                          // reduce lemon inventory
             inventory.money += costLemons               // refund cost of lemon
             updateSupplyView()
+            if lemonMix > inventory.lemons {            // ensure that user isn't capable of mixing supplies they haven't purchased
+                lemonMix = inventory.lemons
+                updateMixView()
+            }
         }
-        println("Inventory: Money = \(inventory.money), Lemons = \(inventory.lemons), Ice = \(inventory.iceCubes)")
+        
     }
     
     @IBAction func icePurchStepperPressed(sender: UIStepper) { // Purchasing Ice Cubes
@@ -86,36 +92,76 @@ class ViewController: UIViewController {
             inventory.iceCubes--                        // reduce ice inventory
             inventory.money += costIce                  // refund cost of ice
             updateSupplyView()
+            if iceMix > inventory.iceCubes {            // ensure that user isn't capable of mixing supplies they haven't purchased
+                iceMix = inventory.iceCubes
+                updateMixView()
+            }
         }
-        println("Inventory: Money = \(inventory.money), Lemons = \(inventory.lemons), Ice = \(inventory.iceCubes)")
     }
     
     @IBAction func lemonMixStepperPressed(sender: UIStepper) { // Mixing Lemons
-        lblMixLemon.text = Int(sender.value).description
+        lemonMix = Int(sender.value)                    // update lemonMix variable
+        updateMixView()
     }
     
     @IBAction func iceMixStepperPressed(sender: UIStepper) { // Mixing Ice Cubes
-        lblMixIce.text = Int(sender.value).description
+        iceMix = Int(sender.value)                      // update iceMix variable
+        updateMixView()
     }
     
     @IBAction func startDayButtonPressed(sender: UIButton) {
         
+        if lemonMix == 0 {                              // Alert user in case they forget to add lemons
+            showMessageAlert(message: "You can't make lemonade without lemons!")
+        }
+        
+        // Subtract Mixed Supplies from Inventory
+        inventory.lemons -= lemonMix
+        inventory.iceCubes -= iceMix
+        
+        // Reset View
+        viewDidLoad()
     }
     
-    // Helper
-    func showMessageAlert(header:String = "Warning", message:String) {
-        var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-        presentViewController(alert, animated: true, completion: nil)
-    }
-    
+    // View Control Functions
     func updateSupplyView() {
+        stepperPurchLemons.value = Double(inventory.lemons)
+        stepperPurchIce.value = Double(inventory.iceCubes)
+        
+        stepperMixLemon.maximumValue = Double(inventory.lemons)
+        stepperMixIce.maximumValue = Double(inventory.iceCubes)
+        
         lblMoney.text = "$\(inventory.money)"
         lblLemons.text = "\(inventory.lemons)"
         lblIce.text = "\(inventory.iceCubes)"
         
-        stepperPurchLemons.value = Double(inventory.lemons)
-        stepperPurchIce.value = Double(inventory.iceCubes)
+        println("Inventory: Money = \(inventory.money), Lemons = \(inventory.lemons), Ice = \(inventory.iceCubes)")
+    }
+    
+    func updateMixView() {
+        lblMixLemon.text = "\(lemonMix)"
+        lblMixIce.text = "\(iceMix)"
+        
+        stepperMixLemon.value = Double(lemonMix)
+        stepperMixIce.value = Double(iceMix)
+        
+        if inventory.lemons > 0{
+            btnStartDay.enabled = true
+        }else{
+            btnStartDay.enabled = false
+            inventory.lemons++                      // add lemon to inventory
+            inventory.money -= costLemons           // reduce total money
+            updateSupplyView()
+            updateMixView()
+        }
+        println("Mix: Lemons: = \(lemonMix), Ice = \(iceMix)")
+    }
+    
+    // Helper Functions
+    func showMessageAlert(header:String = "Warning", message:String) {
+        var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
     }
 }
 
